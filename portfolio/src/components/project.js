247762868bd40d5f0a1adfffc57a536371725a8b
git config --global user.name TNorsang <thenorsang@gmail.com>
2019-05-23 18:43:12 -0400

    added images

--- portfolio/src/components/project.js
+++ portfolio/src/components/project.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Project extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
+    toggleCategories() {
+        if(this.state.activeTab === 0) {
+            return(
+                <div><h1>This is Project 1</h1></div>
+            )
+        }
+    }
+
+  
+
     render() {
         return( 
-            <div><h1> Project Page </h1> </div>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple> 
+                    <Tab>Project 1</Tab>
+                    <Tab>Project 2</Tab>
+                    <Tab>Project 3</Tab>
+                    <Tab>Project 4</Tab>
+                </Tabs>
+                
+                <section>
+                    {this.toggleCategories()}
+                 </section>
+
+            </div>
         )
     }
 }
